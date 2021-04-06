89356fa9bd3de1aeb387ac44242673e88425ab55
Felipe Matheus <felipe.araujo@aluno.uepb.edu.br>
2021-04-06 16:50:37 -0300

    Atualização de design e usabilidade

--- assets/javascript/interactions.js
+++ assets/javascript/interactions.js
@@ -1,8 +1,23 @@
-let items = document.getElementsByClassName("event");
+let items = document.querySelectorAll(".event");
 
 function itemClick(id) {
+    let selectedEvent = document.querySelector("#" + id);
+    let selectedEventContent = selectedEvent.querySelector(".content");
     for (let i = 0; i < items.length; i++) {
-        if(items[i].id == id ) document.getElementById(id).classList.toggle("active");
-        else items[i].classList.remove("active");
+        if(items[i].id == id) {
+            selectedEvent.classList.toggle("active");
+            if (selectedEvent.classList.contains("active")){
+                selectedEventContent.style.maxHeight = selectedEventContent.scrollHeight + "px";
+                setTimeout(() => {  selectedEvent.scrollIntoView({ behavior: 'smooth', block: 'start' }); }, 500);
+            }
+            else {
+                selectedEventContent.style.maxHeight = "0px";
+            }
+        }
+        else {
+            let content = items[i].querySelector(".content");
+            items[i].classList.remove("active");
+            content.style.maxHeight = "0px";
+        }
     }
 }
